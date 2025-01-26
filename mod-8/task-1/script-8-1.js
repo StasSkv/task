90f906422bc64f727006ebdd5c661495ab8d2046
StasSkv <stas000123@gmail.com>
2025-01-26 12:35:17 +0200

    a

---
+++ mod-8/task-1/script-8-1.js
@@ -0,0 +1,70 @@
+const refs = {
+  firstNum: document.querySelector("#first-number"),
+  secondNum: document.querySelector("#second-number"),
+  result: document.querySelector("#result"),
+  errorMessage: document.querySelector("#error-message"),
+  addButton: document.querySelector("#add"),
+  subtractButton: document.querySelector("#subtract"),
+  multiplyButton: document.querySelector("#multiply"),
+  divideButton: document.querySelector("#divide"),
+};
+
+let currentOperation = null;
+
+function updateResult() {
+  const num1 = parseFloat(refs.firstNum.value);
+  const num2 = parseFloat(refs.secondNum.value);
+
+  if (currentOperation === null) {
+    return;
+  } else {
+    calculate(currentOperation, num1, num2);
+  }
+}
+
+function calculate(operation, num1, num2) {
+  let result;
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      if (num2 === 0) {
+        refs.errorMessage.textContent = "На нуль ділити не можна!";
+        refs.result.textContent = "Результат: ";
+        return;
+      }
+      result = num1 / num2;
+      break;
+    default:
+      refs.result.textContent = "Результат: ";
+      return;
+  }
+  refs.result.textContent = `Результат: ${result}`;
+}
+
+refs.addButton.addEventListener("click", () => {
+  currentOperation = "add";
+  updateResult();
+});
+refs.subtractButton.addEventListener("click", () => {
+  currentOperation = "subtract";
+  updateResult();
+});
+refs.multiplyButton.addEventListener("click", () => {
+  currentOperation = "multiply";
+  updateResult();
+});
+refs.divideButton.addEventListener("click", () => {
+  currentOperation = "divide";
+  updateResult();
+});
+
+refs.firstNum.addEventListener("input", updateResult);
+refs.secondNum.addEventListener("input", updateResult);
